b85ce0414b7a919f1b40fcf5eb02d476b2a83be2
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2025-04-03 10:53:37 +0200

    working on vercel configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: "./postcss.config.cjs",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
-  base: '/'
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    minify: "terser",
+    chunkSizeWarningLimit: 1600,
+  },
 });
